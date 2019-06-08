960011e8e7ba256499e6b4648d09b17566c08813
Anton <qwer022022000@mail.ru>
2019-06-08 04:47:05 +0300

    Added two players

--- pong.js
+++ pong.js
@@ -41,6 +41,16 @@ class Ball extends Rect
 	}
 }
 
+class Player extends Rect
+{
+	constructor()
+	{
+		super(20, 100);
+		this.score = 0;
+	}
+
+}
+
 class Pong 
 {
 	constructor(canvas)
@@ -54,6 +64,17 @@ class Pong
 
 		this.ball.vel.x = 100;
 		this.ball.vel.y = 100;
+
+		this.players = [
+			new Player,
+			new Player,
+		];
+
+		this.players[0].pos.x = 40;
+		this.players[1].pos.x = canvas.width - 40;
+		this.players.forEach(player => {
+			player.pos.y = this._canvas.height / 2;
+		});
 
 		let lastTime;
 		const callback = (millis) => {
@@ -71,11 +92,12 @@ class Pong
 		this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
 
 		this.drawRect(this.ball);
+		this.players.forEach(player => this.drawRect(player));
 	}
 	drawRect(rect)
 	{
 		this._context.fillStyle = '#fff';
-		this._context.fillRect(rect.pos.x, rect.pos.y, 
+		this._context.fillRect(rect.left, rect.top, 
 								rect.size.x, rect.size.y);
 	}
 
